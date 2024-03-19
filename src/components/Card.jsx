5902b3f35ee30dcf418f1c4f8dd606fd777d0a76
RohanScripts <shelarrohan1810@gmail.com>
2024-03-19 23:44:32 +0530

    Refactor Card component to remove unnecessary divs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Card() {
-  return (
-    <div>
-      <div className="w-44 h-52 rounded-lg bg-zinc-500"></div>
-    </div>
-  );
+  return <div className="w-44 h-52 rounded-lg bg-zinc-500"></div>;
 }
 
 export default Card;
