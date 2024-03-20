1c3abca16749bdf17163586325eff0fb8f2b8d76
RohanScripts <shelarrohan1810@gmail.com>
2024-03-20 22:25:55 +0530

    Add download button and footer to Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,11 +9,14 @@ function Card() {
       <p className="text-sm">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </p>
-      <div className="flex items-center justify-between absolute footer w-full bottom-0 py-4 left-0 px-3">
+      <div className="flex items-center justify-between absolute footer w-full bottom-0 py-4 left-0 px-3 mb-3">
         <p className="text-xs text-zinc-100">Read More</p>
         <span className="w-5 h-5 bg-zinc-700 rounded-full flex items-center justify-center">
           <FaDownload size="0.6em" color="white" />
         </span>
+      </div>
+      <div className="flex items-center justify-center absolute bottom-0 left-0 bg-green-500 w-full p-1">
+        <h3 className="text-xs font-semibold">Download Now...</h3>
       </div>
     </div>
   );
