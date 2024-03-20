1dd87c6ace6b0ab0490d6195884ac588acc44fc7
RohanScripts <shelarrohan1810@gmail.com>
2024-03-20 09:57:33 +0530

    Add react-icons package and update Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { FaFileAlt } from "react-icons/fa";
+import { FaDownload } from "react-icons/fa6";
 
 function Card() {
-  return <div className="w-44 h-52 rounded-lg bg-zinc-500"></div>;
+  return (
+    <div className="relative text-white w-44 h-52 rounded-[20px] bg-zinc-500 px-5 py-6 overflow-hidden">
+      <FaFileAlt />
+      <p className="text-sm">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+      <div className="flex items-center justify-between absolute footer w-full bottom-0 py-4 left-0 px-3">
+        <p className="text-xs text-zinc-100">Read More</p>
+        <span className="w-5 h-5 bg-zinc-700 rounded-full flex items-center justify-center">
+          <FaDownload size="0.6em" color="white" />
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
