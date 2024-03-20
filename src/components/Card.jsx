b7fa0bb427aec5d86529bd9f0f65e0e82cf3ae48
RohanScripts <shelarrohan1810@gmail.com>
2024-03-20 23:24:37 +0530

    Add framer-motion library and update Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,39 @@
 import React from "react";
 import { FaFileAlt } from "react-icons/fa";
 import { FaDownload } from "react-icons/fa6";
+import { IoIosCloseCircle } from "react-icons/io";
+import { motion } from "framer-motion";
 
-function Card() {
+function Card({ data, reference }) {
   return (
-    <div className="relative text-white w-44 h-52 rounded-[20px] bg-zinc-500 px-5 py-6 overflow-hidden">
+    <motion.div
+      drag
+      dragConstraints={reference}
+      whileDrag={{ scale: 1.2 }}
+      className="relative text-white w-44 h-52 rounded-[20px] bg-zinc-500 px-5 py-6 overflow-hidden border-2 border-black"
+    >
       <FaFileAlt />
-      <p className="text-sm">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </p>
+      <p className="text-sm">{data.description}</p>
       <div className="flex items-center justify-between absolute footer w-full bottom-0 py-4 left-0 px-3 mb-3">
-        <p className="text-xs text-zinc-100">Read More</p>
+        <p className="text-xs text-zinc-100">{data.fileSize}</p>
         <span className="w-5 h-5 bg-zinc-700 rounded-full flex items-center justify-center">
-          <FaDownload size="0.6em" color="white" />
+          {data.close ? (
+            <IoIosCloseCircle />
+          ) : (
+            <FaDownload size="0.6em" color="white" />
+          )}
         </span>
       </div>
-      <div className="flex items-center justify-center absolute bottom-0 left-0 bg-green-500 w-full p-1">
-        <h3 className="text-xs font-semibold">Download Now...</h3>
-      </div>
-    </div>
+      {data.tag.isOpen ? (
+        <div
+          className={`flex items-center justify-center absolute bottom-0 left-0 ${
+            data.tag.color === "blue" ? "bg-blue-500" : "bg-green-500"
+          } w-full p-1`}
+        >
+          <h3 className="text-xs font-semibold">{data.tag.text}</h3>
+        </div>
+      ) : null}
+    </motion.div>
   );
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
 import Card from "./Card";
 
 function Foreground() {
+  const ref = useRef(null);
+  const data = [
+    {
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      fileSize: "12MB",
+      close: true,
+      tag: { isOpen: true, text: "Read More...", color: "green" },
+    },
+    {
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      fileSize: "12MB",
+      close: true,
+      tag: { isOpen: true, text: "Read More...", color: "blue" },
+    },
+    {
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      fileSize: "12MB",
+      close: true,
+      tag: { isOpen: true, text: "Read More...", color: "green" },
+    },
+    {
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      fileSize: "12MB",
+      close: true,
+      tag: { isOpen: true, text: "Read More...", color: "blue" },
+    },
+  ];
+
   return (
-    <div className="fixed top-0 left-0 z-[5] w-full h-full ">
-      <Card />
+    <div
+      ref={ref}
+      className="fixed left-0 z-[5] w-full h-full flex gap-5 p-5 flex-wrap"
+    >
+      {data.map((items, index) => (
+        <Card data={items} reference={ref} />
+      ))}
     </div>
   );
 }
